31e58d6f6c95f9bf245529d302f1307430c562e2
jacob-zawadzki <jtzawadzki22@gmail.com>
2025-12-28 07:28:21 -0600

    Initial commit

---
+++ .config/nvim/lua/plugins/user.lua
@@ -0,0 +1,18 @@
+return {
+  
+  { 
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "tribela/transparent.nvim",
+    auto = true,
+
+  },
+  {
+    "ThePrimeagen/vim-be-good",
+  },
+  
+}
